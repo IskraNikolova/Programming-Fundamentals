743d61c104ab09da47168a10f7b4ad2d495c6d0a
IskraNikolova <air_iskra@yahoo.com>
2016-06-09 11:27:28 +0300

    Add a new projects

--- StringsDictionariesLambdaLINQ/StringsDictionariesLambdaLINQ/ExtractSentencesByKeyword/ExtractSentencesByKeyword.cs
+++ StringsDictionariesLambdaLINQ/StringsDictionariesLambdaLINQ/ExtractSentencesByKeyword/ExtractSentencesByKeyword.cs
@@ -7,7 +7,26 @@ namespace ExtractSentencesByKeyword
         public static void Main()
         {
             string word = Console.ReadLine();
-            string[] sentences = Console.ReadLine().Split(new char[]
+            string[] sentences = Console.ReadLine()
+                .Split(new char[] {'.', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                string[] words = sentences[i].Split();
+                bool isForPrint = false;
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (words[j] == word)
+                    {
+                        isForPrint = true;
+                    }
+                }
+
+                if (isForPrint)
+                {
+                    Console.WriteLine(sentences[i]);
+                }
+            }
         }
     }
 }
